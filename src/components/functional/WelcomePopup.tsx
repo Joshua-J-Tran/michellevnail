f0787d726ba27db028ddc65952ec8708175be5d7
Joss <joshtran2018@gmail.com>
2026-03-20 20:20:53 -0700

    increase heig

--- src/components/functional/WelcomePopup.tsx
+++ src/components/functional/WelcomePopup.tsx
@@ -27,7 +27,7 @@ export default function WelcomePopup() {
         <img
           src="/placeholder/logo.png"
           alt="Michelle V Nails Logo"
-          className="h-130 sm:h-150 object-contain rounded-2xl border-3 border-primary/80"
+          className="h-130 sm:h-170 object-contain rounded-2xl border-3 border-primary/80"
         />
       </div>
     </div>
