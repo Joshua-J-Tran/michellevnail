8703b3a485c31fec50ca92aee5dd84c84cee4040
Joss <joshtran2018@gmail.com>
2026-09-16 18:02:04 -0700

    maybe fix, idk

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/", // Explicitly set base path for SPA routing
   plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
   server: {
     host: true, // to listen on all interfaces
     allowedHosts: ["glorious-gopher-powerful.ngrok-free.app"],
